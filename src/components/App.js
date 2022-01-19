e37b30f5c79ddeb4ac62d81bc2a3228e32250051
kynnedye <90293916+kynnedye@users.noreply.github.com>
2022-01-19 00:52:52 -0500

    added book description and button to add to library

--- src/components/App.js
+++ src/components/App.js
@@ -50,7 +50,7 @@ export default function App(){
         e.preventDefault()
         setSearch(query)
         navigate("/results")
-        console.log(localStorage)
+        setQuery("")
     
    }
    console.log(query)
--- src/components/SearchResults.js
+++ src/components/SearchResults.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Container } from 'react-bootstrap'
+import noImg from "../images/No-image.png"
 
 export default function SearchResults({query, bookSearch}) {
     console.log(bookSearch)
@@ -7,15 +8,24 @@ export default function SearchResults({query, bookSearch}) {
     
         let searchList = bookSearch.map( book => {
             return(
-                <div className="d-flex flex-column w-50 justify-content-center mb-5 book-card">
+                <div key ={book.id}className="d-flex flex-column w-50 justify-content-center mb-5 book-card">
                     <h2>{book.volumeInfo.title}</h2>
                     <p>{book.volumeInfo.authors}</p>
+                    <div className="img-container">
                     <img src={
                          book.volumeInfo.imageLinks === undefined
-                         ? ""
+                         ? `${noImg}`
                          : `${book.volumeInfo.imageLinks.thumbnail}`
                     }
                     />
+                    <div className="overlay">
+                        <h3>Summary:</h3>
+                        <p>{book.volumeInfo.description}</p>
+                        <button className="add">Add to library</button>
+
+                    </div>
+                    </div>
+                   
                 </div>
                 
                     
